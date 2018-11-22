7cbc987b5cf2a237c01b13dbd803163422aceffc
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-22 12:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+  var dwarfNumbers = []
+  for (let i=0; i < dwarves.length; i++){
+    dwarfNumbers = `(i + 1). ${dwarves} `
+  }
+  return = dwarfNumbers.join(" ")
 }
 
 function summonCaptainPlanet(planeteerCalls){
